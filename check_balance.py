7579da2ea7d7c4b1f49d694adaa4c6f71f9eac23
Tuấn Kiệt <votuankiet29012003@gmail.com>
2025-05-19 01:02:19 +0700

    ví

--- check_balance.py
+++ check_balance.py
@@ -1,7 +1,8 @@
 from web3 import Web3
 
 # Thay địa chỉ ví bên dưới bằng ví số 0 mới nhất từ log Ganache Render
-address = "0xc5b22e8042dCC32874a958Cc4881526ef64A4Bc6"
+address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+#address = "0xc5b22e8042dCC32874a958Cc4881526ef64A4Bc6"
 
 w3 = Web3(Web3.HTTPProvider("https://ganache-service-7809.onrender.com/"))
 balance = w3.eth.get_balance(address)
